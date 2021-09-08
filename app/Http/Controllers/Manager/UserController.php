5b09e4dd93807b0d07f3d8f31a9aa30fa75c5310
datToc004 <datdo200499@gmail.com>
2021-09-08 10:55:36 +0700

    manager_user

---
+++ app/Http/Controllers/Manager/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Manager;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(config('restaurant.paginate.user'));
+
+        return view('manager.user.index', compact('users'));
+    }
+
+    public function blockUser($id)
+    {
+        $user = User::findOrFail($id);
+        $user->status = config('restaurant.status_user.block');
+        $user->save();
+
+        return redirect()->route('users.index');
+    }
+
+    public function unBlockUser($id)
+    {
+        $user = User::findOrFail($id);
+        $user->status = config('restaurant.status_user.unblock');
+        $user->save();
+
+        return redirect()->route('users.index');
+    }
+}
